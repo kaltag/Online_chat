c21b36748e3b04fe767e83946f381568fb54f5e9
kaltag <alexx30rus@gmail.com>
2023-01-28 19:11:19 +0400

    notification delete with turbo

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,5 +6,8 @@ class NotificationsController < ApplicationController
   def destroy
     @notification = Notification.find(params[:id])
     @notification.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("notification_id_#{@notification.id}") }
+    end
   end
 end
